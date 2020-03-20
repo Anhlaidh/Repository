a7933600eafd6d6fa38d6673bab1546ae6fd58ca
Anhlaidh <chinahantianzhao@foxmail.com>
2020-03-20 22:32:43 +0800

    do nothing

---
+++ src/HomeWork/Chapter05/BigMultiply.java
@@ -0,0 +1,19 @@
+package HomeWork.Chapter05;
+
+/**
+ * @Description:
+ * @author: Anhlaidh
+ * @date: 2020/3/20 0020 20:02
+ */
+public class BigMultiply {
+
+    public static void main(String[] args) {
+        int a = 1234;
+        int b = 5678;
+        multiply(a, b, 4);
+    }
+
+    private static void multiply(int a, int b, int length) {
+
+    }
+}
